9046f4764b053d0c8efff5f9bc3d21f2a38eb9d5
TrivikramMajjari <tmajjari@gitam.in>
2025-04-24 23:17:05 +0530

    added files

--- backend/src/main/java/com/example/lms/repository/UserRepository.java
+++ backend/src/main/java/com/example/lms/repository/UserRepository.java
@@ -2,7 +2,9 @@ package com.example.lms.repository;
 
 import com.example.lms.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmail(String email);
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
 }
